291fd6064fce1da66d8af765635be7933881bf32
Jumppp_fish <1597859065@qq.com>
2025-03-23 05:14:01 +0000

    Update vite.config.ts for new settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react";
-  
-  // https://vitejs.dev/config/
-  export default defineConfig({
-      plugins: [react()],
-  });
-  
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ["n2l8j8-5173.csb.app"], // 允许的域名
+  },
+});
